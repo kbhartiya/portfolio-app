ec49ef5c962f8605e0e0393d5da511f7320ae0c9
kbhartiya <kbhartiya83@gmail.com>
2020-04-12 03:41:12 +0530

    Updates

---
+++ precache-manifest.992355e00dc7cdd7908b7e4e03c3be80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78831d460a2b709d5cb9a7cd5562fa62",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "f642e30b1c1dce753742",
+    "url": "/portfolio-app/static/css/main.416a8f77.chunk.css"
+  },
+  {
+    "revision": "0cbd8d2722abaf655976",
+    "url": "/portfolio-app/static/js/2.fe92f48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-app/static/js/2.fe92f48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f642e30b1c1dce753742",
+    "url": "/portfolio-app/static/js/main.0bd3d35b.chunk.js"
+  },
+  {
+    "revision": "10efd180a662f5826e5c",
+    "url": "/portfolio-app/static/js/runtime-main.7049b92a.js"
+  },
+  {
+    "revision": "0a4eb1aec2f77f051c803c41ac3a7879",
+    "url": "/portfolio-app/static/media/dust-and-scratches.0a4eb1ae.jpg"
+  },
+  {
+    "revision": "41dfaa73306492a8d784a8cb0eaed95a",
+    "url": "/portfolio-app/static/media/kartik.41dfaa73.jpg"
+  }
+]);
